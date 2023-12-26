fbb406f398cdceeb2ad367880d502af6b3e769ac
yuanwangz <34086226+yuanwangz@users.noreply.github.com>
2023-12-26 13:03:34 +0800

    Update inj-entrypoint.sh

--- root/inj-entrypoint.sh
+++ root/inj-entrypoint.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 sudo rm /tmp/.X0-lock
 
+function set_vnc_password() {
+    if [ ! -z "${VNC_PASSWORD}" ]; then
+        echo "Setting new VNC password."
+        mkdir -p ~/.vnc
+        x11vnc -storepasswd ${VNC_PASSWORD} ~/.vnc/passwd
+    else
+        echo "No NEW_VNC_PASS environment variable set. Using default password."
+    fi
+}
+
 TARGET_AUTO_RESTART=${TARGET_AUTO_RESTART:-no}
 TARGET_LOG_FILE=${TARGET_LOG_FILE:-/dev/null}
 function run-target() {
@@ -14,6 +24,8 @@ function run-target() {
         esac
     done
 }
+set_vnc_password
+
 /entrypoint.sh &
 sleep 5
 inject-monitor &
